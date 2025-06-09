585afd5edecdc3de74819710946e53ce9c4256dd
Junhan Keren <kusumazulaykha@gmail.com>
2025-06-09 23:51:59 +0700

    update coy

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DashboardController;
 use App\Livewire\Pkl\Index as PklIndex;
-use App\Livewire\Pkl\create as PklCreate;
+use App\Livewire\Pkl\Create as PklCreate;
 use App\Livewire\Industri\Index as IndustriIndex;
 use App\Livewire\Industri\Create as IndustriCreate;
 
@@ -11,17 +11,20 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+// Semua route yang butuh login dan validasi user
 Route::middleware(['auth', 'verified', 'check_user_email', 'check_user_role'])
     ->group(function () {
         Route::get('/dashboard', fn () => view('dashboard'))->name('dashboard');
+
+        // Lindungi route industri
+        Route::name('Industri.')->group(function () {
+            Route::get('/industris', IndustriIndex::class)->name('industri.index');
+            Route::get('/industris/create', IndustriCreate::class)->name('industri.create');
+        });
+
+        // Lindungi route pkl
+        Route::name('Pkl.')->group(function () {
+            Route::get('/pkls', PklIndex::class)->name('pkl.index');
+            Route::get('/pkls/create', PklCreate::class)->name('pkl.create');
+        });
     });
-
-
-    Route::name('Industri.')->group(function () {
-        Route::get('/industris', IndustriIndex::class)->name('industri.index');
-});
-
-Route::name('Pkl.')->group(function () {
-    Route::get('/pkls', PklIndex::class)->name('pkl.index');
-});
-
